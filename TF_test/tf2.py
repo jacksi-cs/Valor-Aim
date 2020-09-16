4f38ead1bce912ea3770777c153a4ebb4831e16f
Jack Si <jacksi@utexas.edu>
2020-09-16 12:19:23 -0500

    Changed image name to help keras get_file()

--- TF_test/tf2.py
+++ TF_test/tf2.py
@@ -4,7 +4,16 @@ import tensorflow as tf
 
 def download_images():
 	image_paths = []
-	image_path = "C:\Users\Jack\Desktop\Tensorflow\workspace\training_demo\images\test"
-	image_path = pathlib.Path(image_path)
-	image_paths.append(str(image_path))
+	base_url = 'https://github.com/utjacksi/Valorant-Bot/tree/master/Images/'
+	filenames = ['Screenshot (54).png']
+	
+	for filename in filenames:
+		image_path = tf.keras.utils.get_file(fname = filename, origin = base_url + filename, untar = False)
+		image_path  = pathlib.Path(image_path)
+		image_paths.append(str(image_path))
+	return image_paths
+
+IMAGE_PATHS = download_images()
+
+print(IMAGE_PATHS)
 	

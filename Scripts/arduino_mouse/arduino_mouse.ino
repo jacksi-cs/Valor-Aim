083d1009e3870af4dd2d7a9e0d044850d646c256
Jack Si <jacksi@utexas.edu>
2020-12-14 18:44:32 -0600

    Improved overall performance
    Optimized Arduino code, improved overall detection by modifying RGB bounds

--- Scripts/arduino_mouse/arduino_mouse.ino
+++ Scripts/arduino_mouse/arduino_mouse.ino
@@ -14,40 +14,69 @@ void loop() {
     byte y[2];
     y[1] = Serial.read();
     y[0] = Serial.read();
-    delay(5);
+    delay(1);
     byte x[2];
     x[1] = Serial.read();
     x[0] = Serial.read();
 
     int x_int = int(x[0] << 8 | x[1]);
     int y_int = int(y[0] << 8 | y[1]);
-   //Mouse.move(Serial.read(), Serial.read());
-    move_mouse(x_int, y_int);
+    while(!move_mouse(&x_int, &y_int));
   }
 }
 
-bool move_mouse(int x, int y) {
-  int jump_distance = 127;
-  int x_val = x;
-  int y_val = y;
+bool move_mouse(int *x, int *y) {
+  int jump_distance = 10;
+  int x_val = *x;
+  int y_val = *y;
 
-  while (x_val != 0 && y_val != 0) {
-    if (x_val != 0 && y_val == 0) {
-      int x_move = x_val > 0 ? min(jump_distance, x_val) : max(-jump_distance, x_val);
-      x_val -= x_move;
-      Mouse.move(x_move,0); 
-    }
-    else if (x_val == 0 && y_val != 0) {
-      int y_move = y_val > 0 ? min(jump_distance, y_val) : max(-jump_distance, y_val);
-      y_val -= y_move;
-      Mouse.move(0, y_move);
-    }
-    else if (x_val != 0 && y_val != 0) {
-      int x_move = x_val > 0 ? min(jump_distance, x_val) : max(-jump_distance, x_val);
-      x_val -= x_move;
-      int y_move = y_val > 0 ? min(jump_distance, y_val) : max(-jump_distance, y_val);
-      y_val -= y_move;
-      Mouse.move(x_move, y_move);
-    }  
+  if (x_val != 0 && y_val == 0) {
+    int x_move = x_val > 0 ? min(jump_distance, x_val) : max(-jump_distance, x_val);
+    *x -= x_move;
+    Mouse.move(x_move,0);
+    return false; 
   }
+  else if (x_val == 0 && y_val != 0) {
+    int y_move = y_val > 0 ? min(jump_distance, y_val) : max(-jump_distance, y_val);
+    *y -= y_move;
+    Mouse.move(0, y_move);
+    return false;
+  }
+  else if (x_val != 0 && y_val != 0) {
+    int x_move = x_val > 0 ? min(jump_distance, x_val) : max(-jump_distance, x_val);
+    *x -= x_move;
+    int y_move = y_val > 0 ? min(jump_distance, y_val) : max(-jump_distance, y_val);
+    *y -= y_move;
+    Mouse.move(x_move, y_move);
+    return false;
+  }
+  return true;
 }
+
+//bool move_mouse(int *x, int *y) {
+//  int jump_distance = 10;
+//  int x_val = *x;
+//  int y_val = *y;
+//
+//  if (x_val != 0 && y_val == 0) {
+//    int x_move = x_val > 0 ? min(jump_distance, x_val) : max(-jump_distance, x_val);
+//    x_val -= x_move;
+//    Mouse.move(x_move,0);
+//    return false; 
+//  }
+//  else if (x_val == 0 && y_val != 0) {
+//    int y_move = y_val > 0 ? min(jump_distance, y_val) : max(-jump_distance, y_val);
+//    y_val -= y_move;
+//    Mouse.move(0, y_move);
+//    return false;
+//  }
+//  else if (x_val != 0 && y_val != 0) {
+//    int x_move = x_val > 0 ? min(jump_distance, x_val) : max(-jump_distance, x_val);
+//    x_val -= x_move;
+//    int y_move = y_val > 0 ? min(jump_distance, y_val) : max(-jump_distance, y_val);
+//    y_val -= y_move;
+//    Mouse.move(x_move, y_move);
+//    return false;
+//  }
+//  return true;
+//}
--- Scripts/vision.py
+++ Scripts/vision.py
@@ -1,3 +1,6 @@
+from threading import Thread
+from pynput import keyboard
+
 import cv2 # pip install opencv-python
 import numpy as np
 from mss import mss  # pip install mss==2.0.22
@@ -6,53 +9,49 @@ import time
 import serial # pip install pyserial
 import struct
 
-import keyboard
+# ** CUSTOM SETTINGS **
+detect_width = 254
+detect_height = 254
 
 scripts_on = False
 
-def smooth_move(var):
-    return var - 250
+def smooth_move(var, x_or_y):
+    return int(var - detect_width/2) if (x_or_y == 'x') else int(var - detect_height/2) 
+    #return var - 127
 
-def arduino_conversion(var, x_or_y):
-    diff = -1
-    if x_or_y == 'x':
-        diff = var - 125
-    elif x_or_y == 'y':
-        diff = var - 125
+def arduino_communication(x, y):
+    global scripts_on
+    ser = serial.Serial('COM5', 9600, write_timeout=5)
 
-    #print(diff)
+    if scripts_on:
+        ser.write(struct.pack('h', smooth_move(y, 'y')))
+        ser.write(struct.pack('h', smooth_move(x, 'x')))
 
-    if diff > 127:
-        return 127;
-    elif diff < -127:
-        return -127
-    else:
-        return diff
+# def nothing(x):
+#     pass
 
-def nothing(x):
-    pass
+def aimbot():
+    # time.sleep(5)
+    global detect_width
+    global detect_height
+    mon = {'top': int((1080-detect_height)/2), 'left': int((1920-detect_width)/2), 'width': detect_width, 'height': detect_height}
+    #mon = {'top': 290, 'left': 710, 'width': 500, 'height': 500}
+    #mon = {'top': 415, 'left': 835, 'width': 250, 'height': 250 }
+    sct = mss()
 
-ser = serial.Serial('COM5', 9600, write_timeout=5)
-# time.sleep(5)
+    # Used for determining HSV values
+    # cv2.namedWindow("Trackbars")
+    # cv2.createTrackbar("LOWER H", "Trackbars", 0, 179, nothing)
+    # cv2.createTrackbar("LOWER S", "Trackbars", 0, 255, nothing)
+    # cv2.createTrackbar("LOWER V", "Trackbars", 0, 255, nothing)
+    # cv2.createTrackbar("UPPER H", "Trackbars", 0, 179, nothing)
+    # cv2.createTrackbar("UPPER S", "Trackbars", 0, 255, nothing)
+    # cv2.createTrackbar("UPPER V", "Trackbars", 0, 255, nothing)
 
-mon = {'top': 290, 'left': 710, 'width': 500, 'height': 500}
-#mon = {'top': 415, 'left': 835, 'width': 250, 'height': 250 }
-sct = mss()
+    while True:
+        # print("in while true")
+        #while scripts_on:
 
-# Used for determining HSV values
-# cv2.namedWindow("Trackbars")
-# cv2.createTrackbar("LOWER H", "Trackbars", 0, 179, nothing)
-# cv2.createTrackbar("LOWER S", "Trackbars", 0, 255, nothing)
-# cv2.createTrackbar("LOWER V", "Trackbars", 0, 255, nothing)
-# cv2.createTrackbar("UPPER H", "Trackbars", 0, 179, nothing)
-# cv2.createTrackbar("UPPER S", "Trackbars", 0, 255, nothing)
-# cv2.createTrackbar("UPPER V", "Trackbars", 0, 255, nothing)
-
-while True:
-    if keyboard.is_pressed ("F1"):
-        scripts_on = True
-        print("111 ", scripts_on)
-    while scripts_on:
         # l_h = cv2.getTrackbarPos("LOWER H", "Trackbars")
         # l_s = cv2.getTrackbarPos("LOWER S", "Trackbars")
         # l_v = cv2.getTrackbarPos("LOWER V", "Trackbars")
@@ -61,8 +60,8 @@ while True:
         # u_v = cv2.getTrackbarPos("UPPER V", "Trackbars")
         
         #Ranges for RGB purple (source recommends 250,100,250 with 60 range)
-        lower_range = np.array([200,50,200])
-        upper_range = np.array([300,150,300])
+        lower_range = np.array([190,40,190])
+        upper_range = np.array([310,160,310])
 
         # Ranges for using the trackbars
         # lower_range = np.array([l_h, l_s, l_v])
@@ -88,15 +87,10 @@ while True:
         len1 = len(pixels[1])
 
         if sum0 != 0 or len0 != 0 or sum1 != 0 or len1 != 0:
-            minval = min(pixels[0]) # highest point on the y axis (should be the head)
             min1 = int(round(pixels[0][0])) # minimum value in the array (highest point on the y axis <head>)
             min2 = int(round(pixels[1][0])) # the x coordinate of the lowest y value
             cv2.circle(real_img, (min2, min1+5), 5, (0,255,255), -1)
-            # print("COORD: " ,min1, min2)
-            #ser.write(struct.pack('b', arduino_conversion(min2, 'x')))
-            #ser.write(struct.pack('b', arduino_conversion(min1+5, 'y')))
-            ser.write(struct.pack('h', smooth_move(min1+5)))
-            ser.write(struct.pack('h', smooth_move(min2)))
+            arduino_communication(min2, min1+5)
 
         frame = np.array(real_img)
         # cv2.putText(frame, "FPS: %f" % (1.0 / (time.time() - last_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
@@ -107,6 +101,34 @@ while True:
             cv2.destroyAllWindows()
             break
 
-        if keyboard.is_pressed ("F2"):
-            scripts_on = False
-            print("222 ", scripts_on)
+def scripts_switch():
+    # def on_click(x,y,button,pressed):
+    #     global scripts_on
+    #     if (button == mouse.Button.right and pressed):
+    #         print(1234)
+    #         scripts_on = True
+    #     else:
+    #         print(6543)
+    #         scripts_on = False
+
+    # with mouse.Listener(on_click=on_click) as listener:
+    #     listener.join()
+    def on_press(key):
+        global scripts_on
+        if key == keyboard.Key.caps_lock:
+            print("asdf")
+            scripts_on = not scripts_on
+    
+    # def on_release(key):
+    #     global scripts_on
+    #     if key == keyboard.Key.caps_lock:
+    #         print("1234")
+    #         scripts_on = False
+
+    with keyboard.Listener(
+        on_press=on_press) as listener:
+            listener.join()
+
+
+Thread(target = aimbot).start()
+Thread(target = scripts_switch).start()
